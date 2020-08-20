d8605aef7ff58610709b28e6f4953d921f815596
dany kamel <dany.kamel@esrinea.com>
2020-08-20 21:33:11 +0200

    build

---
+++ build/precache-manifest.de65142fb0ec1a596190ecacd75ad992.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a72367e78c46caf65ff82dde709ed861",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cd0dd17a3d8f8b1de421",
+    "url": "./static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "ae6083dd6cbda91e336c",
+    "url": "./static/js/2.61261b15.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.61261b15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd0dd17a3d8f8b1de421",
+    "url": "./static/js/main.2e933025.chunk.js"
+  },
+  {
+    "revision": "f218d30073fb66bfa9f3",
+    "url": "./static/js/runtime-main.a819eb98.js"
+  }
+]);
