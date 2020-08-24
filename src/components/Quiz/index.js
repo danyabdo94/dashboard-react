953499338f673b5f1282ecca478b45710c75a409
dany kamel <dany.kamel@esrinea.com>
2020-08-24 19:11:22 +0200

    Fix quiz btn size

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import AssessmentIcon from '@material-ui/icons/Assessment';
-import { Button } from '@material-ui/core';
+import { Button, Box } from '@material-ui/core';
 import AssignmentModel from '../../models/AssignmentModel';
 import StaticDataQuizAssignment from '../StaticDataQuizAssignment';
 import './Quiz.scss';
 
 export default function Quiz({ assignment }) {
   return (
-    <div className="quiz">
+    <Box className="quiz">
       <StaticDataQuizAssignment
         icon={<AssessmentIcon color="primary" />}
         assignment={assignment}
       />
-      <Button className="quiz__btn" variant="outlined" color="primary">
-        Start Quiz
-      </Button>
-    </div>
+      <Box>
+        <Button className="quiz__btn" variant="outlined" color="primary">
+          Start Quiz
+        </Button>
+      </Box>
+    </Box>
   );
 }
 
