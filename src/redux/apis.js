02538900a142c03be9d8e6b83c344f6fae038997
dany kamel <dany.kamel@esrinea.com>
2020-08-24 16:17:44 +0200

    Add environment file

--- src/redux/apis.js
+++ src/redux/apis.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default function getData() {
   return axios.get(
-    'https://run.mocky.io/v3/03a30085-0b77-460e-a4ca-464d47b51c57',
+    `${process.env.REACT_APP_API_BASEURL}/03a30085-0b77-460e-a4ca-464d47b51c57`,
     {
       headers: {
         'Content-Type': 'application/json',
