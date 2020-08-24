95fe4d083b2ff41483415fc2ac5ae8e061c61620
dany kamel <dany.kamel@esrinea.com>
2020-08-24 20:32:01 +0200

    Add Exams Time UI

--- src/components/Exams/index.js
+++ src/components/Exams/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import Title from '../Title';
+import './Exams.scss';
+import { Typography, Box, Button } from '@material-ui/core';
 
 export default function Exams() {
   return (
-    <>
-      <Title>Exams Time</Title>
-    </>
+    <div className="exams">
+      <Box className="exams__content">
+        <Typography className="exams__content__title" variant="h5" color="primary">
+          Exams Time
+        </Typography>
+        <Typography className="exams__content__subtitle" variant="p">
+          Here we are, Are you ready to fight?
+          Don&apos;t worry, we prepared some tips to be ready for your exams.
+        </Typography>
+        <Typography className="exams__content__quote" variant="p">
+          <i>
+            &quot;Nothing happens until something moves&quot; ~ Albert Einstein
+          </i>
+        </Typography>
+        <Button className="exams__content__button" variant="contained" color="primary">
+          View exams tips
+        </Button>
+      </Box>
+      <Box className="exams__image">
+        <img alt="exams" src={`${process.env.PUBLIC_URL}/img/exams-image.png`} />
+      </Box>
+    </div>
   );
 }
